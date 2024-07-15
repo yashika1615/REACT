98108a18425267f86e5d7e8d596887669818728c
Yashika1615 <katariay22@gmail.com>
2024-07-15 20:36:21 +0530

    react practise

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 // app.js
 import React from 'react';
 import SomeComponent from './components/Somecomponent.jsx';
-import SomeFuctional from './components/SomeFunctional.jsx';
+// import SomeFuctional from './components/SomeFunctional.jsx';
+
 function App() {
   return (
     <div className="App">
       <SomeComponent/>
-      <SomeFuctional/>
+   
     </div>
   );
 }
--- my-app/src/components/Somecomponent.jsx
+++ my-app/src/components/Somecomponent.jsx
@@ -3,13 +3,26 @@ import React, { Component } from 'react';
 class SomeComponent extends Component {
   constructor() {
     super();
-   }
+    this.state = {
+      count: 0,
+    }
+    this.increment = this.increement.bind(this)
+  }
+  
+  increment() {
+    this.setState(() => ({
+      count: this.state.count + 1,
+   }))
+  }
+
+
   render() {
     return (
-      <div>
-        Hello, I am a class-based component!
+      <div style={{textAlign: 'center', padding:'30px'}}>
+        <button onClick={()=> this.increment()}> Click Me</button>
+        <p>{this.state.count} </p>
       </div>
-    );
+    )
   }
 }
 
